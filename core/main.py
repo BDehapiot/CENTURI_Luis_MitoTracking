0c6557ac66c7b3817795cb613a8ccfb1c8a1a7fd
Benoit Dehapiot <b.dehapiot@gmail.com>
2022-06-02 08:50:24 +0200

    Update main.py

--- core/main.py
+++ core/main.py
@@ -35,7 +35,6 @@ output_list = Parallel(n_jobs=-1)(
     for frame in stack
     )    
 
-# process = np.stack(output_list)
 process = stack.copy()
     
 #%%
@@ -44,21 +43,39 @@ import pims
 import trackpy as tp
 import matplotlib.pyplot as plt
 
-
-f = tp.locate(process[0], 5)
-tp.annotate(f, process[0])
-
-fig, ax = plt.subplots()
-ax.hist(f['mass'], bins=20)
+# f = tp.locate(process[0], 5)
+# fig, ax = plt.subplots()
+# ax.hist(f['mass'], bins=20)
 
 f = tp.locate(process[0], 5, minmass=150)
-tp.annotate(f, process[0])
+# tp.annotate(f, process[0])
+
+for frame in stack:
+    
+    features = tp.locate(frame, 5, minmass=150)
+    
+    
 
 #%% 
 
-test = 
+# from skimage.draw import circle_perimeter_aa
 
+# y_coords = np.array(f.loc[:,'y'])
+# x_coords = np.array(f.loc[:,'x'])
+
+# particle_display = stack[0,...].copy()
+
+# for y_coord, x_coord in zip(y_coords, x_coords):
+    
+#     rr, cc, val = circle_perimeter_aa(int(y_coord), int(x_coord), 11)
+#     particle_display[rr, cc] = val * 65535   
+    
+#%%
+
+coords = np.array(f.loc[:,['y','x']])
+viewer = napari.view_image(stack[0])
+points_layer = viewer.add_points(coords, size=21, symbol='ring')
 
 #%%
 
-viewer = napari.view_image(process)
+# viewer = napari.view_image(particle_display)
